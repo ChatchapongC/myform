0b4e08be379a3c844a016cc5b0145abd8a34d33e
Ingkharat <ingkharat.j@ku.th>
2019-11-20 01:23:00 +0700

    fix models

--- myform/models.py
+++ myform/models.py
@@ -1,13 +1,31 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class Event(models.Model):
-    event_name = models.CharField(max_length=30, primary_key=True)
+class Evaluator(models.Medel):
+    date = models.DateTimeField(auto_now_add=True)
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.question_textgit
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.event_name
+        return self.choice_text
 
+class Event(models.Model):
 
+    event = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.event_text
 
-
+class Answer(models.Model):
+text
+multiple
+boolean
