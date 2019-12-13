79d1dbadf408c85946c66abd7ae66a54146f3a59
Ingkharat <ingkharat.j@ku.th>
2019-12-14 03:03:24 +0700

    fix models

--- myform/models.py
+++ myform/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models
 import datetime
+import User
 
 
 class Event(models.Model):
@@ -14,7 +16,7 @@ class Event(models.Model):
 class Question(models.Model):
     event = models.ForeignKey(Event,on_delete = models.CASCADE)
     question_text = models.CharField(max_length=100)
-    
+    choice_text = models.TextField(blank=True, null=True
     def __str__(self):
         return self.question_text
 
@@ -28,7 +30,9 @@ class Choice(models.Model):
         return self.choice_text
 
 class Evaluator(models.Model):
+    responder = models.ForeignKey(User, on_delete=models.CASCADE)
     event_name = models.ForeignKey(Event, on_delete=models.CASCADE)
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice = models.ForeignKey(Choice,  on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f'{self.event_name}'
+    
