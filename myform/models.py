45f61cff95e898fccca01e945ff85315ebcb2eed
ChatchapongC <chatchapong.c@ku.th>
2019-12-15 15:38:01 +0700

    Edit models

--- myform/models.py
+++ myform/models.py
@@ -4,30 +4,32 @@ from django.contrib.auth.models import User
 import datetime
 
 
-
 class Event(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     event_name = models.CharField(max_length=100)
-    date_of_event = models.DateField(default = timezone.now)
-    
+    date_of_event = models.DateField(default=timezone.now)
+
     def __str__(self):
         return self.event_name
 
+
 class Question(models.Model):
-    event = models.ForeignKey(Event,on_delete = models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
     question_text = models.CharField(max_length=100)
     choice_text = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.question_text
 
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=100)
-    choice_vote = models.IntegerField(default = 0)
+    choice_vote = models.IntegerField(default=0)
 
     def __str__(self):
         return self.choice_text
+
 
 class Evaluator(models.Model):
     responder = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -35,4 +37,3 @@ class Evaluator(models.Model):
 
     def __str__(self):
         return f'{self.event_name}'
-    
